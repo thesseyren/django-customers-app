fd35ae92722c66cf49d8ea44be860bdcb4b73415
Serhat Seyren <thesseyren@gmail.com>
2021-07-17 17:06:55 +0300

    search form for index

--- customers_app/forms.py
+++ customers_app/forms.py
@@ -1,11 +1,22 @@
-from django.forms import ModelForm
+from django import forms
 from localflavor.tr.forms import TRIdentificationNumberField
 
 from .models import Customer
 
-class CustomerForm(ModelForm):
+class CustomerForm(forms.ModelForm):
     tr_id = TRIdentificationNumberField(label="TC Kimlik Numarası")
 
     class Meta:
         model = Customer
-        fields = ["tr_id", "first_name", "last_name", "phone", "province", "district"]
+        fields = "__all__"
+
+class CustomerSearchForm(forms.Form):
+    pk = forms.IntegerField(label="ID", required=False)
+    tr_id = forms.CharField(label="TC Kimlik Numarası", required=False)
+    first_name = forms.CharField(label="Adı", required=False)
+    last_name = forms.CharField(label="Soyadı", required=False)
+    phone = forms.CharField(label="Telefon Numarası", required=False)
+    province = forms.CharField(label="İl", required=False)
+    district = forms.CharField(label="İlçe", required=False)
+
+    page_size = forms.IntegerField(label="Sayfa Boyutu", min_value=1, initial=25)
--- customers_app/views.py
+++ customers_app/views.py
@@ -3,11 +3,45 @@ from django.views import generic
 from django import urls
 
 from .models import Customer
-from .forms import CustomerForm
+from .forms import CustomerForm, CustomerSearchForm
 
 class CustomerListView(generic.ListView):
     model = Customer
-    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.request.GET:
+            self._search_form = CustomerSearchForm(self.request.GET)
+        else:
+            self._search_form = CustomerSearchForm()
+
+        if self._search_form.is_valid():
+            filter_args = {
+                f"{field}__icontains": value
+                for field, value in self._search_form.cleaned_data.items()
+                if value and field != "page_size"
+            }
+            queryset = queryset.filter(**filter_args)
+
+        return queryset
+
+    def get_paginate_by(self, *args, **kwargs):
+        if self._search_form.is_valid():
+            return self._search_form.cleaned_data["page_size"]
+        else:
+            return 25
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = self._search_form
+
+        queries = self.request.GET.copy()
+        if "page" in queries:
+            del queries["page"]
+        context["queries"] = queries
+
+        return context
 
 class CustomerCreateView(generic.edit.CreateView):
     model = Customer
