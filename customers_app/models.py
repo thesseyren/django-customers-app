17df2f784b46b401dafe5bb876ca29046926b0fe
Serhat Seyren <thesseyren@gmail.com>
2021-07-17 13:47:55 +0300

    phone number validation

--- customers_app/models.py
+++ customers_app/models.py
@@ -2,11 +2,16 @@ from django.db import models
 from django import urls
 from localflavor.tr.tr_provinces import PROVINCE_CHOICES
 
+from .validators import phone_number_validator
+
 class Customer(models.Model):
     tr_id = models.CharField("TC Kimlik Numarası", max_length=11, unique=True)
     first_name = models.CharField("Adı", max_length=50)
     last_name = models.CharField("Soyadı", max_length=50)
-    phone = models.CharField("Telefon Numarası", max_length=10)
+    phone = models.CharField(
+        "Telefon Numarası", max_length=10,
+        validators=[phone_number_validator],
+        help_text="Başında 0 olmadan 10 haneli ve bitişik biçimde girin.")
     province = models.CharField("İl", max_length=2, choices=PROVINCE_CHOICES)
     district = models.CharField("İlçe", max_length=20)
 
---
+++ customers_app/validators.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+
+def phone_number_validator(value):
+    if not value.isnumeric():
+        raise ValidationError("Telefon numarası sadece rakamlardan oluşmalı.")
+
+    if len(value) != 10:
+        raise ValidationError("Telefon numarası tam 10 hane olmalı.")
+
+    if value[0] == "0":
+        raise ValidationError("Telefon numarası 0 rakamı ile başlayamaz.")
